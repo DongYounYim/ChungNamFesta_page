aa4df49375634a97a172801e11e6a2a81b5b798e
DongYounYim <dyyim4725@gmail.com>
2021-10-21 00:34:09 +0900

    frist commit

---
+++ Popup/pop_up.js
@@ -0,0 +1,6 @@
+// Just popup on main page
+
+window.onload =function () {
+  //Poster size width=500, height=700 (px) 사이즈 값에 맞게 창 조절
+   window.open("./Popup/popup.html",  "popupNo1", "width=470, height=720, left=200, top=200");
+}
