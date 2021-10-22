a1e27316e56b3a7224845a4d3351b951a18643fe
DongYounYim <dyyim4725@gmail.com>
2021-10-23 01:28:21 +0900

    Revise some html_link and pop_up image

--- Popup/pop_up.js
+++ Popup/pop_up.js
@@ -2,5 +2,5 @@
 
 window.onload =function () {
   //Poster size width=500, height=700 (px) 사이즈 값에 맞게 창 조절
-   window.open("./Popup/popup.html",  "popupNo1", "width=470, height=720, left=200, top=200");
+   window.open("./Popup/popup.html",  "popupNo1", "width=470, height=710, left=200, top=200");
 }
